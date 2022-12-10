83a45b5aee53a617c15a5846a69687605dcabe22
William Boswall <wboswall@outlook.com>
2022-12-10 19:43:09 -0400

    Commit frontEndWebDev

---
+++ frontEndWebDev/bptsiSite/js/taterPwr.js
@@ -0,0 +1,8 @@
+/*
+ *
+ * Can't use alert function since browsers such as Chrome and Edge flags them ...
+ * as violations since it slows sites' response time to a minimum.
+ *
+ */
+
+//alert("Hello from TaterPwr.js My ECAMA Scripting engine");
---
+++ frontEndWebDev/demodotwboswalldotca/demo/js/tater.js
@@ -0,0 +1,5 @@
+function testJS()
+{
+    document.getElementById("test").innerHTML="Hello world! This is a test! from Javascript!";
+    //alert("Hello World!");
+}
