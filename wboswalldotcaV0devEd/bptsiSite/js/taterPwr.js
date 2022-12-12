2a5e32da536d7371f94fa3ebb1875001a3a463f0
William Boswall <weboswall@GOV.PE.CA>
2022-12-12 09:49:47 -0400

    Commit Housekeeping

--- wboswalldotcaV0devEd/bptsiSite/js/taterPwr.js
+++
@@ -1,8 +0,0 @@
-/*
- *
- * Can't use alert function since browsers such as Chrome and Edge flags them ...
- * as violations since it slows sites' response time to a minimum.
- *
- */
-
-//alert("Hello from TaterPwr.js My ECAMA Scripting engine");
