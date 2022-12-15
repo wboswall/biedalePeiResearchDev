d12d86c66ee5da18d965d58fc22e15d8563cc354
William Boswall <wboswall@live.com>
2022-12-15 13:31:41 -0400

    Commit MaplePak CSS/SCSS & JS Project v0.0.0.1Dev 12/15/2022 1:31PM

---
+++ frontEndWebDev/maplepak/mapleJS/mapleEngine.js
@@ -0,0 +1,14 @@
+/*Project MaplePak v0.0.0.1devEd
+ *Date Created: 12/15/2022 12:52PM
+ *Date Modified: 12/15/2022 12:53PM
+ *Official Version: V0.0
+ *Project Purpose: MaplePak takes the stress out of styling and functionality by simply letting
+ * 				   the user efficiently and quickly a website by just concentrating on the content.  
+ * 				   However, will have to choose one's own colour scheme for own website. 
+ *maplepak.js file purpose:MaplePak's JS is written in pure vanilla JS.  This is was deliberate because JS is not secure but more to the point loosely secure.  
+ CSS was written in SCSS and compiled into css.  This is the main css file links are placed.  
+*/
+function displayBox()
+{
+	document.getElementById.innerHTML="Hello";	
+}
